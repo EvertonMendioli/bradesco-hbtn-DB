c60b7384d75a8b7394ee2c971b67653ca32fb04d
EvertonMendioli <evertonbaixista@gmail.com>
2025-08-20 13:05:37 +0000

    jpa_hibernate

---
+++ jpa_hibernate/administrativo-api/src/main/java/entities/Pessoa.java
@@ -0,0 +1,52 @@
+package entities;
+
+public class Pessoa {
+
+    private int id;
+    private String nome;
+    private String email;
+    private int idade;
+    private long cpf;
+    private String data_nascimento;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public long getCpf() {
+        return cpf;
+    }
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+
+    
+
+
+}
---
+++ jpa_hibernate/administrativo-api/src/main/java/models/PessoaModel.java
@@ -0,0 +1,52 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import entities.Pessoa;
+
+public class PessoaModel {
+
+    public void create(Pessoa p) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            System.out.println("Iniciando a transação");
+            em.getTransaction().begin();
+            em.persist(p);
+            em.getTransaction().commit();
+            System.out.println("Pessoa criado com sucesso !!!");
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao criar o Pessoa !!!" + e.getMessage());
+        } finally {
+            em.close();
+            System.out.println("Finalizando a transação");
+        }
+    }
+
+    public void update(Pessoa p) {
+        // TODO
+    }
+
+    public void delete(Pessoa p) {
+        // TODO
+    }
+
+    public Pessoa findById(Pessoa p) {
+        // TODO
+        return null;
+    }
+
+    public List<Pessoa> findAll() {
+
+        List<Pessoa> Pessoas = new ArrayList<Pessoa>();
+        // TODO
+        return null;
+    }
+}
