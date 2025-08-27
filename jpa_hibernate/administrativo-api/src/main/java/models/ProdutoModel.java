06784bf4e54b99aa0016f2b785333f4f93f8347e
EvertonMendioli <evertonbaixista@gmail.com>
2025-08-26 22:13:15 -0300

    produto

--- jpa_hibernate/administrativo-api/src/main/java/models/ProdutoModel.java
+++ jpa_hibernate/administrativo-api/src/main/java/models/ProdutoModel.java
@@ -31,21 +31,95 @@ public class ProdutoModel {
     }
 
     public void update(Produto p) {
-        // TODO
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            System.out.println("Iniciando a transação");
+            
+            Produto prd = em.find(Produto.class, p.getId());
+            prd.setNome(p.getNome());
+            prd.setPreco(p.getPreco());
+            prd.setQuantidade(p.getQuantidade());
+            prd.setStatus(p.getStatus());
+            em.getTransaction().begin();
+            em.merge(prd);
+            em.getTransaction().commit();
+            System.out.println("Produto alterado com sucesso !!!");
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao alterar o produto !!!" + e.getMessage());
+        } finally {
+            em.close();
+            System.out.println("Finalizando a transação");
+        }
+
     }
 
     public void delete(Produto p) {
-        // TODO
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            System.out.println("Iniciando a transação");
+            
+            Produto prd = em.find(Produto.class, p.getId());
+            em.getTransaction().begin();
+            em.remove(prd);
+            em.getTransaction().commit();
+            System.out.println("Produto exclído com sucesso !!!");
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao excluir o produto !!!" + e.getMessage());
+        } finally {
+            em.close();
+            System.out.println("Finalizando a transação");
+        }
     }
 
     public Produto findById(Produto p) {
-        // TODO
-        return null;
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
+        EntityManager em = emf.createEntityManager();
+        Produto prd = new Produto();
+        try {
+            System.out.println("Iniciando a transação");
+            prd = em.find(Produto.class, p.getId());
+            
+           
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao alterar o produto !!!" + e.getMessage());
+        } finally {
+            em.close();
+            System.out.println("Finalizando a transação");
+        }
+        return prd;
     }
 
     public List<Produto> findAll() {
-
         List<Produto> produtos = new ArrayList<Produto>();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("admin-jpa");
+        EntityManager em = emf.createEntityManager();
+        Produto prd = new Produto();
+        try {
+            System.out.println("Iniciando a transação");
+            prd = em.find(Produto.class);
+            prd = em.
+            
+           
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao alterar o produto !!!" + e.getMessage());
+        } finally {
+            em.close();
+            System.out.println("Finalizando a transação");
+        }
+        return prd;
+
+
+
+
+        
         // TODO
         return null;
     }
