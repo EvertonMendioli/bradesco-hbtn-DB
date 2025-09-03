b0de3675801ef391a5f9ad3d59719565efeffb8c
EvertonMendioli <evertonbaixista@gmail.com>
2025-09-03 09:28:47 -0300

    Schema

---
+++ java_jpa_hibernate_mapeamento/sql_schema_database_admin.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Aluno (id  integer, email varchar(255), matricula varchar(255), nascimento datetime, nomeCompleto varchar(255), curso_id bigint, primary key (id))
+CREATE TABLE Curso (id  integer, nome varchar(255), sigla varchar(255), material_id bigint, professor_id bigint, primary key (id), unique (material_id))
+CREATE TABLE Endereco (id  integer, bairro varchar(255), cep varchar(255), cidade varchar(255), endereco varchar(255), estado varchar(255), logradouro varchar(255), numero varchar(255), aluno_id bigint, primary key (id))
+CREATE TABLE MaterialCurso (id  integer, url varchar(255), primary key (id))
+CREATE TABLE Professor (id  integer, email varchar(255), matricula varchar(255), nomeCompleto varchar(255), primary key (id))
+CREATE TABLE Telefone (id  integer, DDD varchar(255), numero varchar(255), aluno_id bigint, primary key (id))
